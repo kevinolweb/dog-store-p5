72329c01a0cb940b94556cff4fb343845bddb0fc
kevinolweb <kevin.oleary91@gmail.com>
2023-07-11 16:03:21 +0000

    savings bolt added to show special offers

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'price',
+        'image',
+        'in_stock'
+    )
+    ordering = ('name',)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Policies)
 admin.site.register(ProductStatus)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def all_products(request):
+    products = Product.objects.all()
+    product_list = {
+    'products':products,
+    }
+    return render(request,'products/all-products.html',product_list)
