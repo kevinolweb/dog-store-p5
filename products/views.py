4507eee8a07f016d64f52dc83172616dae138e86
kevinolweb <kevin.oleary91@gmail.com>
2023-07-12 21:55:19 +0000

    Search working

--- products/views.py
+++ products/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect,reverse
+from django.db.models import Q
 from .models import *
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
+    query=None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query=request.GET['q']
+            if not query:
+                messages.error(request,"You never entered a search term!")
+                return redirect(reverse,'all-products-list')
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products=products.filter(queries)
+
     product_list = {
     'products':products,
+    'search_term':query,
     }
     return render(request,'products/all-products.html',product_list)
 
