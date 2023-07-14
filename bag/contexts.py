9c075a9796fabd1504b0c472337948954201533b
kevinolweb <kevin.oleary91@gmail.com>
2023-07-14 16:18:23 +0000

    contexts.py issues being bugged

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,18 +1,34 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
     bag_items=[]
     product_quantity=0
     total=0
 
+    bag = request.session.get('bag',{})
+    
+    for product_id, quantity in bag.items():
+        product = get_object_or_404(Product,pk=product_id)
+        total += product.price
+        product_quantity = quantity
+        bag_items.append({
+            'product_id':product_id,
+            'quantity':quantity,
+            'product':product,
+        }
+        )
+
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENT)
         free_delivery_left = settings.FREE_DELIVERY_THRESHOLD - total
     
     else:
-        delivery=0
-        free_delviery_left=0
+        delivery=0 
+        free_delivery_left=0
 
     grand_total = total + delivery
 
--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 def view_bag(request):
     #views whats in bag
 
     return render(request,'bag/bag.html')
+
+def add_to_bag(request,product_id):
+
+    quantity = request.POST.get('quantity')
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag',{})
+
+    if product_id in list(bag.keys()):
+        bag[product_id] +=quantity
+    else:
+        bag[product_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
