7c55f7818564df5dc2a23597ab3185b1065f208c
kevinolweb <kevin.oleary91@gmail.com>
2023-09-03 16:29:06 +0000

    signals and checkout form added

--- dogstore/settings.py
+++ dogstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't54ut#_*05g88&y_pk=!u&*^4#&uupvdzdw#v--(f^dgzk)q+c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-kevinolweb-dogstorep5-7si6xlgtyzo.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['8000-kevinolweb-dogstorep5-alkjb46ksdf.ws-eu104.gitpod.io']
 
 
 # Application definition
