911470abc27a40a37565206074c04ff978966a9e
kevinolweb <kevin.oleary91@gmail.com>
2023-09-04 20:44:04 +0000

    Successful payment submitting in Stripe and Stripe JS added and configured in views

--- dogstore/settings.py
+++ dogstore/settings.py
@@ -168,5 +168,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# stripe payment processing
 FREE_DELIVERY_THRESHOLD=50
 STANDARD_DELIVERY_PERCENT=8
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
