9b03a6aae1e37de72cc7ca3f41d780231806d62a
kevinolweb <kevin.oleary91@gmail.com>
2023-09-05 18:32:54 +0000

    added form for profile to edit

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
-
+from .forms import UserProfileForm
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The was profile was updated successfully')
 
-    template = 'profiles/profile.html'
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
-    return render(request, template, context)
+    return render(request, 'profiles/profile.html', context)
