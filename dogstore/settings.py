6995d233f5536dbcc833a99237ffb59f51ceb236
kevinolweb <kevin.oleary91@gmail.com>
2023-09-06 23:04:19 +0000

    initial deployment

--- dogstore/settings.py
+++ dogstore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 't54ut#_*05g88&y_pk=!u&*^4#&uupvdzdw#v--(f^dgzk)q+c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-kevinolweb-dogstorep5-5svvc0p5llg.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['dogbeds-2f2abe44d7a8.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -115,13 +116,17 @@ WSGI_APPLICATION = 'dogstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
