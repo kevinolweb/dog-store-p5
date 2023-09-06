a0619bc51c6e2f5acc03f136d959456fe31dd3d0
kevinolweb <kevin.oleary91@gmail.com>
2023-09-06 23:38:44 +0000

    skey removed and debug false

--- dogstore/settings.py
+++ dogstore/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't54ut#_*05g88&y_pk=!u&*^4#&uupvdzdw#v--(f^dgzk)q+c'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dogbeds-2f2abe44d7a8.herokuapp.com', 'localhost']
 
